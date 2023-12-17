dd6c74de389507995680fc0ceb4c523c5b178629
SuJeet Mehra <82982529+sujeetmehra01@users.noreply.github.com>
2023-12-17 10:24:02 +0530

    index.js Setup & MongoDB Setup & Connecting to the Database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,32 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+// initialize server instance
+const app = express();
+
+// middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// cors for cross origin resource sharing
+app.use(cors());
+
+// mongoose connection
+// YlBd2JmMCIJIIQGY
+// mongodb+srv://sm829564:YlBd2JmMCIJIIQGY@memories-blog.a5kbzr3.mongodb.net/memories-blog
+const CONNECTION_URL =
+  "mongodb+srv://sm829564:YlBd2JmMCIJIIQGY@memories-blog.a5kbzr3.mongodb.net/memories-blog";
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection established"))
+  .catch((error) => console.log(error.message));
+
+// listen to port
+const PORT = process.env.PORT || 5000;
+
+// listen to server
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
